adf0fdb73a6796d5b0231a455fba0daeceb8901b
SheLesTT <lovsevdenis3513@gmail.com>
2023-07-23 19:38:58 +0300

    hexagon sprites in map_visualization

--- map_visualization.py
+++ map_visualization.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from math import *
 
 pygame.init()
 window_size = (800, 600)
@@ -7,14 +8,28 @@ screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Drawing Polygons on a Sprite")
 clock = pygame.time.Clock()
 
-class PolygonSprite(pygame.sprite.Sprite):
-    def __init__(self, color, vertices, initial_position):
+class Hexagon(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, color = (70,70,120), width = 70, height = 70):
         super().__init__()
-        self.image = pygame.Surface((100, 100), pygame.SRCALPHA)  # Create a blank surface with transparency
-        pygame.draw.polygon(self.image, color, vertices)
-        self.rect = self.image.get_rect(topleft=initial_position)
-        # self.image.fill((0, 0, 0, 0))
-        # pygame.draw.polygon(self.image, color, self.vertices)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)  # Create a blank surface with transparency
+        # pygame.draw.polygon(self.image, color, vertices)
+
+        self.rect = self.image.get_rect(center=(pos_x, pos_y))
+
+        self.rect.x = pos_x
+        self.rect.y = pos_y
+        # self.rect.center = (pos_x, pos_y)
+        self.points = []
+        self.image.fill((0, 0, 0, 0))
+        v = 0
+        for i in range(6):
+            self.points.append((cos(v)*((self.rect.width//2)-2)+ self.rect.width/2,
+                                sin(v)*((self.rect.height//2)-2)+ self.rect.height/2))
+            v += (pi*2)/6
+        pygame.draw.polygon( self.image, (30,70,50),self.points )
+
+        print(self.points)
+
         self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
@@ -28,11 +43,11 @@ polygon_vertices = [(0, 0), (50, 100), (100, 0)]  # Triangle vertices
 sprite_position = (200, 200)
 
 # Create the polygon sprite object
-polygon_sprite = PolygonSprite(polygon_color, polygon_vertices, sprite_position)
+hex = Hexagon(50,50,)
 
 # Create a sprite group
-all_sprites = pygame.sprite.Group()
-all_sprites.add(polygon_sprite)
+hexes = pygame.sprite.Group()
+hexes.add(hex)
 
 running = True
 while running:
@@ -43,18 +58,19 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             # Get the mouse position
             mouse_x, mouse_y = pygame.mouse.get_pos()
+            print(mouse_x, mouse_y)
 
-            for sprite in all_sprites:
+            for sprite in hexes:
                 if sprite.rect.collidepoint(mouse_x, mouse_y):
                     local_x = mouse_x - sprite.rect.x
                     local_y = mouse_y - sprite.rect.y
                     if sprite.mask.get_at((local_x, local_y)):
                         print("Clicked")
 
-    all_sprites.update()
+    hexes.update()
 
     screen.fill((255, 255, 255))
-    all_sprites.draw(screen)
+    hexes.draw(screen)
     pygame.display.flip()
     clock.tick(60)
 
