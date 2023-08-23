efab054c5046cd968fbe391b385329eb576016ce
SheLesTT <lovsevdenis3513@gmail.com>
2023-08-23 16:12:51 +0300

    unit movement created

--- MouseClickHandler.py
+++ MouseClickHandler.py
@@ -24,9 +24,11 @@ class MouseClickHandler:
             sprite_clicked = self.check_if_hex_is_clicked(event)
             if sprite_clicked and self.unit_selected :
                 self.selected_sprite.remove_unit()
-                self.unit_selected = None
-                sprite_clicked.add_unit(self.unit_selected)
+                self.unit_selected.grid_pos_x = sprite_clicked.grid_pos_x
+                self.unit_selected.grid_pos_y = sprite_clicked.grid_pos_y
 
+                sprite_clicked.add_unit(self.unit_selected)
+                self.unit_selected = None
 
     def check_if_hex_is_clicked(self, event):
         mouse = pygame.math.Vector2(event.pos)
