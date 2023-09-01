a6c1a9820d8306da04ea1d805081c9f8dc6deb38
SheLesTT <lovsevdenis3513@gmail.com>
2023-09-01 14:05:16 +0300

    sending objects instead of strings

--- internet_acsess/Server.py
+++ internet_acsess/Server.py
@@ -1,6 +1,8 @@
 import socket
 from _thread import *
-import  sys
+import sys
+from net_player import Player
+import pickle
 
 server = ""
 
@@ -16,37 +18,29 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
+players = [Player(0, 0, 10, 10, (255, 0, 0)), Player(0, 0, 10, 10, (0, 255, 0))]
 
-def read_pos(string):
-    stritng = string.split(",")
-    return int(stritng[0]), int(stritng[1])
 
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0,0),(100,100)]
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps((players[player])))
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
-
-
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
             if not data:
                 print("Disconected")
                 break
             else:
                 if player == 0:
-                    reply = pos[1]
+                    reply = players[1]
                 else:
-                    reply = pos[0]
+                    reply = players[0]
                 #
                 # print("Received: ", data)
                 # print("Sending: ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
 
@@ -58,5 +52,5 @@ current_player = 0
 while True:
     conn, addr = s.accept()
     print("Connected to: " + addr[0] + ":" + str(addr[1]))
-    start_new_thread(threaded_client, (conn,current_player))
-    current_player +=1
+    start_new_thread(threaded_client, (conn, current_player))
+    current_player += 1
