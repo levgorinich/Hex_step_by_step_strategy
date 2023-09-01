96d8c5cc0ab14891b172444c3496d51ba33f7000
SheLesTT <lovsevdenis3513@gmail.com>
2023-09-01 13:36:30 +0300

    simple client server ready

--- internet_acsess/Server.py
+++ internet_acsess/Server.py
@@ -16,29 +16,47 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-def threaded_client(conn):
-    conn.send(str.encode("Welcome to the server"))
-    reply = ""
+
+def read_pos(string):
+    stritng = string.split(",")
+    return int(stritng[0]), int(stritng[1])
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+pos = [(0,0),(100,100)]
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
+
+
 
             if not data:
                 print("Disconected")
                 break
             else:
-                print("Received: ", reply)
-                print("Sending: ", reply)
+                if player == 0:
+                    reply = pos[1]
+                else:
+                    reply = pos[0]
+                #
+                # print("Received: ", data)
+                # print("Sending: ", reply)
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
         except:
             break
 
     print("Lost connection")
     conn.close()
 
+
+current_player = 0
 while True:
     conn, addr = s.accept()
     print("Connected to: " + addr[0] + ":" + str(addr[1]))
-    start_new_thread(threaded_client, (conn,))
+    start_new_thread(threaded_client, (conn,current_player))
+    current_player +=1
--- map_visualization.py
+++ map_visualization.py
@@ -15,12 +15,13 @@ clock = pygame.time.Clock()
 #creating window
 window_size = (1280, 720)
 screen = pygame.display.set_mode(window_size)
+
 internal_surface_size = (2500,2500)
 
 # creating main game classes
 game_map = Map(25,25)
 user_interface = UI(window_size, game_map)
-tracker = MapMovementTracker(internal_surface_size, pygame.display.get_surface().get_size(), )
+tracker = MapMovementTracker(internal_surface_size, window_size, )
 renderer = Render(internal_surface_size, map_movement_tracker=tracker, user_interface=user_interface)
 click_handler = MouseClickHandler(game_map, user_interface, tracker)
 
