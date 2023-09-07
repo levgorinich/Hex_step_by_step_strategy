0d5d5411481e4288b0a3e957c12113e286818b80
levgorinich <vadik.vadim2003v@yandex.ru>
2023-09-07 18:51:12 +0300

    asd

--- MouseClickHandler.py
+++ MouseClickHandler.py
@@ -1,7 +1,7 @@
 import pygame
-
+from mover import Mover
 class MouseClickHandler:
-    def __init__(self, game_map, User_interface, tracker):
+    def __init__(self,game_map,  User_interface, tracker):
         self.user_interface = User_interface
         self.game_map = game_map
         self.selected_sprite = None
@@ -9,6 +9,7 @@ class MouseClickHandler:
         self.unit_selected = None
         self.tracker = tracker
         self.was_clicked = False
+        self.mover = Mover(self.game_map)
         pass
 
 
@@ -39,15 +40,19 @@ class MouseClickHandler:
             self.sprite_clicked = self.check_if_hex_is_clicked(event)
             if self.check_if_hex_is_clicked(event) and self.unit_selected :
 
+                starting_sprite = self.selected_sprite.grid_pos
+                ending_sprite = self.sprite_clicked.grid_pos
+                self.mover.move(starting_sprite, ending_sprite)
+
                 # if defending_unit := sprite_clicked.unit_on_hex:
-                if defending_unit := self.sprite_clicked.unit_on_hex:
-                    atacking_unit = self.unit_selected
-                    
-                    self.handle_fighting(atacking_unit,defending_unit)
-
-                else:
-
-                    self.move_unit()
+                # if defending_unit := self.sprite_clicked.unit_on_hex:
+                #     atacking_unit = self.unit_selected
+                #
+                #     self.handle_fighting(atacking_unit,defending_unit)
+                #
+                # else:
+                #
+                #     self.move_unit()
 
 
 
@@ -75,52 +80,52 @@ class MouseClickHandler:
                     return sprite
         return None
 
-    def handle_fighting(self, atacking_unit, defending_unit):
-        if atacking_unit.grid_pos != defending_unit.grid_pos:
-            self.kill_all(atacking_unit, defending_unit)
-            self.kill_enemy(atacking_unit, defending_unit)
-            self.kill_yourself(atacking_unit, defending_unit)
-            self.kill_nothing(atacking_unit, defending_unit)
-        else:
-            pass
-
-    def kill_all(self, atacking_unit, defending_unit):
-        if (defending_unit.health_bar.hp - atacking_unit.attack <= 0
-            and atacking_unit.health_bar.hp - defending_unit.attack <= 0):
-            self.selected_sprite.kill_unit()
-            self.sprite_clicked.kill_unit()
-            print("double death")
-
-    def kill_enemy(self, atacking_unit, defending_unit):
-        if (defending_unit.health_bar.hp - atacking_unit.attack <= 0
-            and atacking_unit.health_bar.hp - defending_unit.attack > 0):
-            self.sprite_clicked.kill_unit()
-            self.unit_selected.update(defending_unit.attack)
-            self.unit_selected.grid_pos = self.sprite_clicked.grid_pos
-
-            self.sprite_clicked.add_unit(self.unit_selected)
-            self.selected_sprite.remove_unit()
-            self.unit_selected = None
-
-    def kill_yourself(self, atacking_unit, defending_unit):
-        if (defending_unit.health_bar.hp - atacking_unit.attack > 0
-            and atacking_unit.health_bar.hp - defending_unit.attack <= 0):
-            defending_unit.update(atacking_unit.attack)
-            self.selected_sprite.kill_unit()
-
-    def kill_nothing(self, atacking_unit, defending_unit):
-        if (defending_unit.health_bar.hp - atacking_unit.attack > 0
-            and atacking_unit.health_bar.hp - defending_unit.attack > 0):
-            defending_unit.update(atacking_unit.attack)
-            self.unit_selected.update(defending_unit.attack)
-            print("last case")
-
-    def move_unit(self):
-
-        self.selected_sprite.remove_unit()
-        self.unit_selected.grid_pos = self.sprite_clicked.grid_pos
-        self.sprite_clicked.add_unit(self.unit_selected)
-        self.unit_selected = None
-
-
-
+    # def handle_fighting(self, atacking_unit, defending_unit):
+    #     if atacking_unit.grid_pos != defending_unit.grid_pos:
+    #         self.kill_all(atacking_unit, defending_unit)
+    #         self.kill_enemy(atacking_unit, defending_unit)
+    #         self.kill_yourself(atacking_unit, defending_unit)
+    #         self.kill_nothing(atacking_unit, defending_unit)
+    #     else:
+    #         pass
+    #
+    # def kill_all(self, atacking_unit, defending_unit):
+    #     if (defending_unit.health_bar.hp - atacking_unit.attack <= 0
+    #         and atacking_unit.health_bar.hp - defending_unit.attack <= 0):
+    #         self.selected_sprite.kill_unit()
+    #         self.sprite_clicked.kill_unit()
+    #         print("double death")
+    #
+    # def kill_enemy(self, atacking_unit, defending_unit):
+    #     if (defending_unit.health_bar.hp - atacking_unit.attack <= 0
+    #         and atacking_unit.health_bar.hp - defending_unit.attack > 0):
+    #         self.sprite_clicked.kill_unit()
+    #         self.unit_selected.update(defending_unit.attack)
+    #
+    #         self.unit_selected.grid_pos = self.sprite_clicked.grid_pos
+    #
+    #         self.sprite_clicked.add_unit(self.unit_selected)
+    #         self.unit_selected = None
+    #
+    # def kill_yourself(self, atacking_unit, defending_unit):
+    #     if (defending_unit.health_bar.hp - atacking_unit.attack > 0
+    #         and atacking_unit.health_bar.hp - defending_unit.attack <= 0):
+    #         defending_unit.update(atacking_unit.attack)
+    #         self.selected_sprite.kill_unit()
+    #
+    # def kill_nothing(self, atacking_unit, defending_unit):
+    #     if (defending_unit.health_bar.hp - atacking_unit.attack > 0
+    #         and atacking_unit.health_bar.hp - defending_unit.attack > 0):
+    #         defending_unit.update(atacking_unit.attack)
+    #         self.unit_selected.update(defending_unit.attack)
+    #         print("last case")
+    #
+    # def move_unit(self):
+    #
+    #     self.selected_sprite.remove_unit()
+    #     self.unit_selected.grid_pos = self.sprite_clicked.grid_pos
+    #     self.sprite_clicked.add_unit(self.unit_selected)
+    #     self.unit_selected = None
+    #
+    #
+    #
---
+++ mover.py
@@ -0,0 +1,71 @@
+import pygame
+
+
+class Mover():
+    def __init__(self, game_map):
+        self.game_map = game_map
+        self.starting_sprite = None
+        self.ending_sprite = None
+        self.atacking_unit = None
+        self.defending_unit = None
+
+    def move(self, hex_start: tuple[int, int], hex_end: tuple[int, int]):
+        self.starting_sprite = self.game_map.hexes.hexes_dict[hex_start]
+        self.ending_sprite = self.game_map.hexes.hexes_dict[hex_end]
+
+        if self.starting_sprite.unit_on_hex and self.ending_sprite.unit_on_hex:
+            self.atacking_unit = self.starting_sprite.unit_on_hex
+            self.defending_unit = self.ending_sprite.unit_on_hex
+
+        elif unit := self.starting_sprite.unit_on_hex:
+
+            self.starting_sprite.remove_unit()
+            unit.grid_pos = hex_end
+            self.ending_sprite.add_unit(unit)
+
+    def handle_fighting(self, atacking_unit, defending_unit):
+        if atacking_unit.grid_pos != defending_unit.grid_pos:
+            self.kill_all(atacking_unit, defending_unit)
+            self.kill_enemy(atacking_unit, defending_unit)
+            self.kill_yourself(atacking_unit, defending_unit)
+            self.kill_nothing(atacking_unit, defending_unit)
+        else:
+            pass
+
+    def kill_all(self, atacking_unit, defending_unit):
+        if (defending_unit.health_bar.hp - atacking_unit.attack <= 0
+                and atacking_unit.health_bar.hp - defending_unit.attack <= 0):
+            self.ending_sprite.kill_unit()
+            self.ending_sprite.kill_unit()
+            print("double death")
+
+    def kill_enemy(self, atacking_unit, defending_unit):
+        if (defending_unit.health_bar.hp - atacking_unit.attack <= 0
+                and atacking_unit.health_bar.hp - defending_unit.attack > 0):
+            self.ending_sprite.kill_unit()
+            self.unit_selected.update(defending_unit.attack)
+
+            self.unit_selected.grid_pos = self.ending_sprite.grid_pos
+
+            self.ending_sprite.add_unit(self.unit_selected)
+            self.unit_selected = None
+
+    def kill_yourself(self, atacking_unit, defending_unit):
+        if (defending_unit.health_bar.hp - atacking_unit.attack > 0
+                and atacking_unit.health_bar.hp - defending_unit.attack <= 0):
+            defending_unit.update(atacking_unit.attack)
+            self.ending_sprite.kill_unit()
+
+    def kill_nothing(self, atacking_unit, defending_unit):
+        if (defending_unit.health_bar.hp - atacking_unit.attack > 0
+                and atacking_unit.health_bar.hp - defending_unit.attack > 0):
+            defending_unit.update(atacking_unit.attack)
+            self.unit_selected.update(defending_unit.attack)
+            print("last case")
+
+    def move_unit(self):
+
+        self.ending_sprite.remove_unit()
+        self.unit_selected.grid_pos = self.ending_sprite.grid_pos
+        self.ending_sprite.add_unit(self.unit_selected)
+        self.unit_selected = None
