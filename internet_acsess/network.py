d11f1286c50f9d35227aaa44a41b3c14b31ad15f
SheLesTT <lovsevdenis3513@gmail.com>
2023-11-26 21:05:06 +0300

    map size selection added

--- internet_acsess/network.py
+++ internet_acsess/network.py
@@ -5,7 +5,7 @@ class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # self.client.setblocking(False)
-        self.server = "5.42.78.110"
+        self.server = "localhost"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p, self.seed, self.players_amount = self.connect()
